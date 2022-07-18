48fdfb4fbc4f49860b945c332b47d7d56ceac062
Stellaa24 <stellamaris241@gmail.com>
2022-07-18 09:02:08 -0300

    exercicio calculadora dio

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        //Calculadora
+        System.out.println("Ecercicio calculadora");
+        Calcula.soma(3, 6);
+        Calcula.subtracao(9, 1.8);
+        Calcula.multiplicacao(7, 8);
+        Calcula.divisao(5, 2.5);
+
+        //Mensagem
+        System.out.println("Exercicio Mensagem");
+        Mensagem.obterMensagem(hora: 9);
+        Mensagem.obterMnesagem(hora: 14);
+        Mensagem.obterMnesagem(hora 1);
+
+    }
+}
