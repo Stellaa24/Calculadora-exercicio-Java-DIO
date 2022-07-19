9e024eb51dd6c7fcaef7cb8d4081e7a66722b810
Stellaa24 <stellamaris241@gmail.com>
2022-07-19 08:36:29 -0300

    exercicio calculadora dio

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int i1 = 10;
+        int i2 = 20;
+
+
+        System.out.println("i1==i1 " + (i1 == i2));
+        System.out.println("i1 != i2 " + (i1 != i2));
+        System.out.println("i1 <= i2" + (i1 <= i2));
+
+    }
+
+}
